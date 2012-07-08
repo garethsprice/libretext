1b58a55ccbfb06a519eece69740045ca59d77f9f
Gareth Price <gareth@readysetrocket.com>
2012-07-08 13:43:09 -0400

    Setting case sensitivity in Javascript

--- javascript/script.js
+++ javascript/script.js
@@ -28,6 +28,34 @@ $(document).ready(function() {
 		  $.each(response.wordlist, function(index, data) {
 				$('#wordlist').append(data + ' ');
 			});
+			setCase();
     }});
 	}
+	
+	// Change the case in real-time without regenerating the list
+	$('input[name=transformcase],select[name=transformcaseto]').change(function() {
+		setCase();	
+	});
+	
+	function setCase() {
+		if(!$('input[name=transformcase]').is(':checked')) {
+			return;
+		}
+		
+		switch($('select[name=transformcaseto]').val()) {
+			case 'upper':
+				$('#wordlist').text($('#wordlist').text().toUpperCase());
+				break;
+			case 'lower':
+				$('#wordlist').text($('#wordlist').text().toLowerCase());
+				break;	
+			case 'title':
+				$('#wordlist').text($('#wordlist').text().toProperCase());
+				break;
+		}
+	}
 });
+
+String.prototype.toProperCase = function () {
+    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
