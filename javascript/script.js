652f5565206951ae23e04abb565e6d56d1928117
Gareth Price <gareth@readysetrocket.com>
2012-07-08 12:41:15 -0400

    Added checks to parse JSON objects correctly if the server returns text/plain

--- javascript/script.js
+++ javascript/script.js
@@ -11,6 +11,10 @@ $(document).ready(function() {
 	
 	function initializeLanguages() {
 		$.ajax({url: 'lib/languages.json', type:'GET', success: function(languages) {
+			// Check that we got an object back. If the remote server
+			// is misconfigured to return JSON as text/plain we need to
+			// parse the JSON object manually.
+			if(typeof languages !== "object") languages = $.parseJSON(languages);
 			$.each(languages, function(index, language) {
 				$('select[name=language] optgroup').append('<option value=' + index + '>' + language.name + '</option>');
 			});
@@ -19,6 +23,7 @@ $(document).ready(function() {
 	
 	function submitForm() {
 		$.ajax({url: 'api/', type:'POST', data: $('#wordlistgenerator').serialize(), success: function(response) {
+			if(typeof response !== "object") response = $.parseJSON(response);
 			$('#wordlist').html('');
 		  $.each(response.wordlist, function(index, data) {
 				$('#wordlist').append(data + ' ');
